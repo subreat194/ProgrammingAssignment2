51846098be420b6db1e2ad192dc6fc7cac325e0b
subreat194 <sahu.subrat194@gmail.com>
2015-05-17 09:22:20 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
 # makeCacheMatrix is a function that returns a list of functions
 # Its puspose is to store a martix and a cached value of the inverse of the 
 # matrix. Contains the following functions:
-# * setMatrix      set the value of a matrix
-# * getMatrix      get the value of a matrix
+# * setMat      set the value of a matrix
+# * getMat      get the value of a matrix
 # * cacheInverse   get the cahced value (inverse of the matrix)
 # * getInverse     get the cahced value (inverse of the matrix)
 #
